b2d9322360479ae028bf17c7c6b35ca39a3dbe33
Fabio Murillo <fabiomurillo2020@outlook.com>
2022-04-20 17:15:30 -0500

    creacion de clase spiderman con atributos

---
+++ app/spiderclass.js
@@ -0,0 +1,11 @@
+class spiderman {
+    constructor(nombre,edad,actor,numeropeliculas,estudio) {
+        this.nombre = nombre
+        this.edad = edad 
+        this.actor = actor
+        this.numeropeliculas = numeropeliculas
+        this.estudio = estudio
+    }
+};
+
+module.exports = spiderman;
